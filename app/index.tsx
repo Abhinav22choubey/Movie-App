4aad719d2fa73b2cbfa28843ae570cbc7282369c
Abhinav kumar choubey <abhinavchoubey22@gmail.com>
2025-10-21 19:19:42 +0530

    start building group bottom

--- app/index.tsx
+++ app/index.tsx
@@ -5,8 +5,7 @@ export default function Index() {
   return (
     <View  className="flex-1 justify-center items-center">
       <Text className="text-5xl font-bold text-dark-200 " >Welcome !</Text>
-        <Link href="/onboarding">Onboarding</Link>
-        <Link href="/movie/avengers">Avengers: Age of Ultron</Link>
+
     </View
       >
   );
