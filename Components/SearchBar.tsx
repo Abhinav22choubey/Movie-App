fdfea6a65901999f41bed37ebaec0b5bd43a0bd1
Abhinav kumar choubey <abhinavchoubey22@gmail.com>
2025-10-23 23:08:18 +0530

    added serach  TMDB API

--- Components/SearchBar.tsx
+++ Components/SearchBar.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import {View, Text, StyleSheet, Image, TextInput} from 'react-native';
 import {icons} from "@/constants/icons";
 
-const SearchBar = () => {
+interface Props {
+    placeholder: string;
+    onpress?: () => void;
+}
+
+
+const SearchBar = ({ placeholder , onPress }:Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4 ">
       <Image source={icons.search} className="size-5"
              resizeMode="contain"  tintColor="#ab8bff"/>
-      <TextInput onPress={()=>{}}
-        placeholder={"Search"}
+      <TextInput onPress={onPress}
+        placeholder={placeholder}
         value=""
         onChangeText={(text: string) => {}}
         placeholderTextColor="#a8b5db"
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,17 +3,23 @@ import {Link} from "expo-router";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
 import SearchBar from "@/Components/SearchBar";
+import {useRouter} from "expo-router";
 
 
 export default function Index() {
-  return (
+
+    const router = useRouter();
+    return (
     <View className="flex-1 bg-primary" >
         <Image source={images.bg} className="absolute w-full z-0"/>
         <ScrollView className="flex-1 px-5">
             <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"></Image>
 
             <View className="flex-1 mt-5"   >
-                <SearchBar/>
+                <SearchBar
+                    onPress={()=>{router.push("/search")}}
+                    placeholder={"Search for a movie"}
+                />
             </View>
         </ScrollView>
     </View>
