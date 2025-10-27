939408d33ce2c089e12fc5a4f8e95f4be6bf9c13
Abhinav kumar choubey <abhinavchoubey22@gmail.com>
2025-10-27 11:14:23 +0530

    hook useFetch

---
+++ services/useFetch.ts
@@ -0,0 +1,40 @@
+// fetchMovies
+// fetchMoviesDetails
+
+// useFetch(fetchMovies)
+
+import {useCallback, useEffect, useState} from "react";
+
+const useFetch=<T>(fetchFunction :()=>Promise<T>, autoFetch=true)=>{
+    const [data,setData]=useState<T  |  null>(null);
+    const [loading,setLoading]=useState(false);
+    const [error,setError]=useState< Error | null>(null);
+
+    const fetchData=async (data)=>{
+        try{
+            setLoading(true);
+            setError(null);
+
+            const result=await fetchFunction();
+            setData(result);
+        }
+        catch(err){
+            setError(err instanceof Error ? err : new Error('An error occurred'));
+        }
+        finally{
+            setLoading(false);
+        }
+    }
+    const reset=()=>{
+        setData(null);
+        setLoading(false);
+        setError(null);
+    }
+    useEffect(()=>{
+        if(autoFetch){
+            fetchData();
+        }
+    },[]);
+    return {loading,error,data,refetch:fetchData,reset};
+}
+export default useFetch;
